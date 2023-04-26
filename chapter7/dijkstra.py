4feffbd219293e91305910c49741f9a975c01ba4
joamatheus <joaquimatheus1@gmail.com>
2023-04-26 20:40:02 -0300

    chapter7 dijkstra

---
+++ chapter7/dijkstra.py
@@ -0,0 +1,13 @@
+grafos = []
+
+grafo['voce'] = ['alice', 'bob', 'claire']
+
+grafo['inicio'] = {}
+grafo['inicio']['a'] = 6
+grafo['inicio']['b'] = 2
+
+grafo['a'] = 3
+grafo['b']['a'] = 3
+grafo['b']['fim'] = 5
+
+grafo['fim'] = {}
